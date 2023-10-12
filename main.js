81a58a93e2fa076078b65df1a838d2bb4c31fba2
Your Name <rebecca.solomon446@gmail.com>
2023-10-12 20:49:47 +0100

    fixes delete row feature

--- main.js
+++ main.js
@@ -1,6 +1,10 @@
 const formEl = document.querySelector("form");
 const tbodyEl = document.querySelector("tbody");
+
 const tableEl = document.querySelector("table");
+
+const budget = document.getElementById("monthly-budget");
+const grossBudget = document.getElementById("gross-budget");
 // function to add user input to table
 function addInfoToTable(e) {
   e.preventDefault();
@@ -10,7 +14,7 @@ function addInfoToTable(e) {
   <tr>
       <td>${item}</td>
       <td>${budgetVal}</td>
-      <td><button classList="delete-btn">Delete</button></td>
+      <td><button class="delete-btn">Delete</button></td>
   </tr>
   `
 }
@@ -18,12 +22,25 @@ function addInfoToTable(e) {
 // function to delete row
 function deleteRow(e) {
   if(!e.target.classList.contains("delete-btn")){
-    null;
-    
+    return;
   }
   const btn = e.target;
   btn.closest("tr").remove(); 
 }
 
+// function to update budget value
+function updateBudget() {
+  let sum = 0;
+  const userTotalBudget = document.getElementById("total-budget").value;
+  
+  const inputBudget = document.getElementById("budget-num").value;
+  const finalBudget = document.getElementById("gross-budget");
+  // after final budget value is inputted once, it is removed from the form??
+
+  sum += Number(inputBudget);
+  finalBudget.innerHTML = `Remaining Budget : ${userTotalBudget - sum}`
+}
+
 formEl.addEventListener("submit", addInfoToTable);
+formEl.addEventListener("submit", updateBudget);
 tableEl.addEventListener("click", deleteRow);
